ef04aa6d0b00531295d87417079d4764ab6373e6
Eric Smith <eric.c.smith@sbcglobal.net>
2020-09-17 17:45:41 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -10,7 +10,7 @@ def find_min_in_nested_arrays(src)
     element_index = 0 
     while element_index < src[row_index].count do
       if src[row_index][element_index] <= min_daily_temp
-        min_daily_temp = 
+        min_daily_temp = src[row_index][element_index]
     end
   end
 
